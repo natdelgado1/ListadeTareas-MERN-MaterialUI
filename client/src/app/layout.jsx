b453a3aa9aa9e4f3a22bcfcb6e88d462ef0c602d
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2024-04-24 13:13:05 -0400

    Se formateó

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -20,9 +20,7 @@ export default function RootLayout({ children }) {
         <div style={{flex:1, marginTop: "70px"}}>
         {children}         
         </div>
-        </div>
-        
-
+        </div>        
       </body>
     </html>
   );
