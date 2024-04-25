35c376f9e550e332610a6df6deeb8f09243785e7
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2024-04-25 15:09:53 -0400

    Se pusieron estilos para el top nav

--- client/src/components/navs/TopNavList.jsx
+++ client/src/components/navs/TopNavList.jsx
@@ -1,20 +1,23 @@
-"use client"
-import { CheckBoxOutlined, TableRowsOutlined } from "@mui/icons-material";
-import Link from 'next/link'
+"use client";
+import Link from "next/link";
 import { Fragment } from "react";
 
-const TopNavList = () =>{
-    return(
-        <Fragment>
-            <div className="flex text-[#4D4D4D] gap-3 py-2">
-        <Link href={"/list"} >
-          <CheckBoxOutlined /> List
-        </Link>
-        <Link href={"/table"}>
-          <TableRowsOutlined /> Table
-        </Link>
-            </div>
-      </Fragment>  
-    )
-}
+const TopNavList = () => {
+  return (
+    <div className="flex text-[#4D4D4D] gap-3 py-2 border-b-[3px] ">
+      <Link
+        className="px-6 rounded-md py-1 hover:bg-gray-200 font-semibold"
+        href={"/list"}
+      >
+        List
+      </Link>
+      <Link
+        className="px-6 rounded-md py-1 hover:bg-gray-200 font-semibold"
+        href={"/table"}
+      >
+        Manage
+      </Link>
+    </div>
+  );
+};
 export default TopNavList;
