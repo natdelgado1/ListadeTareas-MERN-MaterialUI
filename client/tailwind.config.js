61d2ccc8ea94f44f8e9e5976ca63f8ebc97e2430
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2024-04-25 02:39:47 -0400

    Se agreg]o una configuración para checkbox

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  variants: {
+    extend: {
+      backgroundColor: ['checked'],
+      borderColor: ['checked'],
+    }
+  },
   theme: {
     extend: {
       backgroundImage: {
