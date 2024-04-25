588a43a13506ecc2e4595f791b08407592857f1b
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2024-04-25 04:00:30 -0400

    Se agregó la propiedad name al modelo

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -4,6 +4,10 @@ const uniqueValidator = require("mongoose-unique-validator");
 
 const UserSchema = new mongoose.Schema(
   {
+    name: {
+      type: String,
+      required: true,
+    },
     email: {
       type: String,
       required: true,
