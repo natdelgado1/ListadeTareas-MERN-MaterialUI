fc7165600946afd1b797ad5b8e7a6634b911e5ea
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2024-05-05 19:17:20 -0400

    feat: Agregar componente CommentOnTask para comentarios en las tareas

--- client/src/components/alerts/CommentOnTask.jsx
+++ client/src/components/alerts/CommentOnTask.jsx
@@ -1,7 +1,31 @@
-const CommentOnTask = () =>{
-    return(
+import { PaperAirplaneIcon } from "@heroicons/react/24/outline";
 
-    )
-}
+const CommentOnTask = ({ commentPosition }) => {
+  const handleSumit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <form
+      onSubmit={handleSumit}
+      className="rounded-lg px-4 py-3 text-sm bg-white absolute z-50 h-fit shadow-lg border-1 border-gray-400"
+      style={{ top: `${commentPosition.y + 190}px`, right: `10px` }}
+    >
+      <div>
+        <textarea
+          className="block outline-none text-sm rounded-md w-full"
+          placeholder="Comentario"
+          id="comment"
+          name="comment"
+          rows="3"
+          //   value={comment}
+          //   onChange={handleChange}
+        ></textarea>
+      </div>
+      <div className="flex justify-end">
+        <button type="submit"><PaperAirplaneIcon width={24} height={24} />{" "}</button>
+      </div>
+    </form>
+  );
+};
 
 export default CommentOnTask;
