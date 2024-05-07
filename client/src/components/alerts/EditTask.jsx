7095ace364d1b245854c2ef880b263c018e9aa41
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2024-05-07 12:54:30 -0400

    Se formateó el código

--- client/src/components/alerts/EditTask.jsx
+++ client/src/components/alerts/EditTask.jsx
@@ -1,5 +1,5 @@
 "use client";
-import {  updateTask } from "@/app/api/route";
+import { updateTask } from "@/app/api/route";
 import { FlagIcon } from "@heroicons/react/24/outline";
 import moment from "moment/moment";
 import { useEffect } from "react";
@@ -22,43 +22,41 @@ const EditTask = ({ task, cancelEditTask, setShowEditTask }) => {
   );
   const handleSubmit = async (e) => {
     e?.preventDefault();
-    if(taskId){
-        console.log(taskId);
-    const data = {
-      title: taskTitle,
-      description: taskDescription,
-      priority: taskPriority,
-      status: taskStatus,
-      taskDate: moment().set({
-        dates: taskDate.split("-")[2],
-        months: parseInt(taskDate.split("-")[1]) - 1,
-        y: taskDate.split("-")[0],
-      }),
-      deadline: moment().set({
-        dates: taskDeadline.split("-")[2],
-        months: parseInt(taskDeadline.split("-")[1]) - 1,
-        y: taskDeadline.split("-")[0],
-      }),
-    };
-    try {
-      const result = await updateTask(taskId, data);
-      const temTasks = [result, ...tasks];
-      setTasks(temTasks);
-      console.log(result);
-      setTaskTitle(task?.title || "");
-      setTaskDescription(task?.description || "");
-      setTaskPriority(task?.priority || "");
-      setTaskStatus(task?.status || "");
-      setTaskdeadline(deadline.format("YYYY-MM-DD"));
-      setTaskDate(date.format("YYYY-MM-DD"));
-      setShowEditTask(false);
-    } catch (error) {
-      console.log("Error al actualizar la tarea:", error);
-    }}
+    if (taskId) {
+      console.log(taskId);
+      const data = {
+        title: taskTitle,
+        description: taskDescription,
+        priority: taskPriority,
+        status: taskStatus,
+        taskDate: moment().set({
+          dates: taskDate.split("-")[2],
+          months: parseInt(taskDate.split("-")[1]) - 1,
+          y: taskDate.split("-")[0],
+        }),
+        deadline: moment().set({
+          dates: taskDeadline.split("-")[2],
+          months: parseInt(taskDeadline.split("-")[1]) - 1,
+          y: taskDeadline.split("-")[0],
+        }),
+      };
+      try {
+        const result = await updateTask(taskId, data);
+        const temTasks = [result, ...tasks];
+        setTasks(temTasks);
+        console.log(result);
+        setTaskTitle(task?.title || "");
+        setTaskDescription(task?.description || "");
+        setTaskPriority(task?.priority || "");
+        setTaskStatus(task?.status || "");
+        setTaskdeadline(deadline.format("YYYY-MM-DD"));
+        setTaskDate(date.format("YYYY-MM-DD"));
+        setShowEditTask(false);
+      } catch (error) {
+        console.log("Error al actualizar la tarea:", error);
+      }
+    }
   };
-
- 
- 
 
   return (
     <div>
@@ -112,7 +110,7 @@ const EditTask = ({ task, cancelEditTask, setShowEditTask }) => {
             </div>
           </div>
           <div className="flex fonline-none m-3 focus: border-black">
-            <div >
+            <div>
               <label>Priority</label>
               <select
                 type="text"
@@ -131,7 +129,7 @@ const EditTask = ({ task, cancelEditTask, setShowEditTask }) => {
               </select>
             </div>
             <div>
-                <label>Status</label>
+              <label>Status</label>
               <select
                 className="px-2 py-2"
                 type="text"
