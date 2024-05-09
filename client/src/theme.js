8b4efdbabc847acb7b4472c7a0208b839851d50e
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2024-05-08 22:25:36 -0400

    Creación de nuevo tema para el login

--- client/src/theme.js
+++ client/src/theme.js
@@ -3,20 +3,31 @@ import { createTheme } from "@mui/material";
 import { purple } from "@mui/material/colors";
 
 export const theme = createTheme({
-    palette: {
-      primary: {
-        main: purple[500],
-      },
-      secondary: {
-        main: "#f44336",
-      },
+  palette: {
+    primary: {
+      main: purple[500],
     },
-  });
+    secondary: {
+      main: "#f44336",
+    },
+  },
+});
+export const theme2 = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    background: {
+      paper: '#ba68c8',
+    },
+  },
+  
+});
 
-  export const fondoLila = createTheme({
-    palette: {
-      background: {
-        paper: '#fef9ff', // your color
-      },
+export const fondoLila = createTheme({
+  palette: {
+    background: {
+      paper: '#fef9ff', // your color
     },
-  });
+  },
+});
