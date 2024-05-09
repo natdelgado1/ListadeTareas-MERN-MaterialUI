5552371fbd48ab22730d9c21f64f8f6c7bf3c06a
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2024-05-08 22:27:43 -0400

    Se agrego el sidebar al layout de las paginas de list y board

--- client/src/app/(list, table & progress)/layout.jsx
+++ client/src/app/(list, table & progress)/layout.jsx
@@ -1,12 +1,22 @@
-
+"use client";
+import Sidebar from "@/components/navs/Sidebar";
+import TopNav from "@/components/navs/TopNav";
 import TopNavList from "@/components/navs/TopNavList";
+import FilterProvider from "@/context/FilterContext";
 
 
 const MainLayout = ({ children }) => {
   return (
     <div className="px-4">
-      <TopNavList/>
-      {children}
+      <div style={{ display: "flex" }}>
+        <FilterProvider>
+          <Sidebar />
+          <div style={{ flex: 1, marginTop: "20px" }}>
+            <TopNav/> {children}
+          </div>
+        </FilterProvider>
+      </div>
+      <TopNavList />
     </div>
   );
 };
