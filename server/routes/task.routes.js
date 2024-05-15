4b98366b5a560eb55961e87afcf771da2003c34c
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2024-05-15 17:13:48 -0400

    Update task.routes.js to include authentication for all routes

--- server/routes/task.routes.js
+++ server/routes/task.routes.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const taskController = require("../controllers/task.controller");
 const { authenticate } = require('../config/jwt.config');
 
-router.post("", taskController.createTask);
-router.get("", taskController.findAllTasks);
-router.get("/:id", taskController.findTask);
-router.put("/update-status/:id", taskController.updateStatus);
-router.put("/update-description/:id", taskController.updateDescription);
-router.put("/:id", taskController.updateTask);
-router.delete("/:id", taskController.deleteTask);
+router.post("", authenticate, taskController.createTask);
+router.get("", authenticate, taskController.findAllTasks);
+router.get("/:id", authenticate, taskController.findTask);
+router.put("/update-status/:id", authenticate, taskController.updateStatus);
+router.put("/update-description/:id", authenticate, taskController.updateDescription);
+router.put("/:id", authenticate, taskController.updateTask);
+router.delete("/:id", authenticate, taskController.deleteTask);
 //Filtros de tareas
-router.get("/filter/:dateType", taskController.getFilteredTasks);
+router.get("/filter/:dateType", authenticate, taskController.getFilteredTasks);
 router.get("/filter/:dateType/pending",authenticate, taskController.getFilteredTaskinStatusPending);
 
 module.exports = router;
