7ebf19fcd8cc6c2ae755a3a7320de4ad1a27b1bc
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2024-05-15 17:14:18 -0400

    Formateo de código

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const userController = require('../controllers/user.controller');
 const { authenticate } = require('../config/jwt.config');
 
+
 /* Recuperacion Password */
 router.get("/passwordReset", userController.passwordResetToken);
 router.patch("/passwordReset", userController.passwordReset);
+
 
 /* Rutas Basicas del CRUD */
 router.post("", userController.createUser);
