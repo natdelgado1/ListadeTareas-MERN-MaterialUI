a1520dead3f07c8b38ff94ce0033baf3feb7ec58
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2024-05-15 16:52:37 -0400

    Adicion de UserProvider a RootLayout para el contexto del usuario

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -1,20 +1,16 @@
+"use client"
 import { Inter } from "next/font/google";
 import "./globals.css";
-import TopNav from "@/components/navs/TopNav";
-import Sidebar from "@/components/navs/Sidebar";
-import FilterProvider  from "@/context/FilterContext";
+import { UserProvider } from "@/context/UserContext";
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "Lista de Tareas",
-  description: "App básica para lista de tareas",
-};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>         
+          <UserProvider>
           {children}
+          </UserProvider>
       </body>
     </html>
   );
